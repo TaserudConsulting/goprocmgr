b6fb9c8bfad7f5a2f4ff20358d9aa2e380591f87
Elis Hirwing <elis@hirwing.se>
2022-11-12 13:51:34 +0100

    main/serve: Write out listening address

--- src/commands/serve/main.go
+++ src/commands/serve/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/etu/goprocmgr/src/config"
 	"github.com/gorilla/mux"
@@ -12,6 +13,8 @@ import (
 func Serve() {
 	router := newRouter()
 	config := config.Get(false)
+
+	fmt.Fprintln(os.Stderr, "Listening on", fmt.Sprintf("%s:%d", config.Settings.ListenAddress, config.Settings.ListenPort))
 
 	// Listen to configured address and port.
 	log.Fatal(http.ListenAndServe(
@@ -23,6 +26,7 @@ func Serve() {
 func newRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 
+	// Web server endpoints
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./static/index.html")
 	})
