c6816def162c6bff33ffe1ba99e23b135e036a2a
Elis Hirwing <elis@hirwing.se>
2023-01-12 19:06:23 +0100

    main/serve: Add endpoint to fetch the current config

--- serve.go
+++ serve.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -18,7 +18,7 @@ func (serve *Serve) Run(config *Config) {
 
 	router := serve.newRouter()
 
-	fmt.Fprintln(os.Stderr, "Listening on", fmt.Sprintf("http://%s:%d", serve.config.Settings.ListenAddress, serve.config.Settings.ListenPort))
+	log.Printf("Listening on http://%s:%d\n", serve.config.Settings.ListenAddress, serve.config.Settings.ListenPort)
 
 	// Listen to configured address and port.
 	log.Fatal(http.ListenAndServe(
@@ -40,6 +40,13 @@ func (serve *Serve) newRouter() *mux.Router {
 	router.HandleFunc("/web/script.js", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./static/script.js")
 	})
+
+	// This endpoint is served at GET /api/config and it returns the
+	// currently loaded config.
+	router.HandleFunc("/api/config", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(serve.config)
+	}).Methods("GET")
 
 	return router
 }
