65dd467b262f8a4827ab1717fb69318d2ed043ec
Elis Hirwing <elis@hirwing.se>
2023-03-03 15:21:24 +0100

    main/web: Add a JS function to fetch current state for main menu renderings

--- static/script.js
+++ static/script.js
@@ -6,6 +6,32 @@
     const nav = root.appendChild(document.createElement(`nav`))
     const main = root.appendChild(document.createElement(`div`))
 
+    //
+    // This function will query the config and runner API's to get the
+    // configured servers and then to determine which ones are running
+    // or not. It should be used to render a list of processes and
+    // display if it's running or not.
+    //
+    const fetchServers = async() => {
+        const response = {};
+
+        const configs = await (await fetch('/api/config')).json()
+        const runners = await (await fetch('/api/runner')).json()
+
+        // Go through configs to build response containing some info
+        // about the service and it's current status.
+        for (const serverName in configs.servers) {
+            response[serverName] = {
+                name: serverName,
+                cmd: configs.servers[serverName].cmd,
+                cwd: configs.servers[serverName].cwd,
+                isRunning: serverName in runners,
+            }
+        }
+
+        return response
+    }
+
     nav.textContent = "navigation"
     nav.id = "nav"
     main.textContent = "main viewer"
